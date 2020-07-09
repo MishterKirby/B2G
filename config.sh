9f78995f8800b62ef116814b79f29391e7b370f3
MishterKirby <48867925+MishterKirby@users.noreply.github.com>
2020-07-08 19:25:49 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -108,7 +108,7 @@ repo_sync $1
 	echo Valid devices to configure are:
 	echo - emulator-10-arm
 	echo - emulator-10-x86_64
-	echo - sargo (Google Pixel 3a)
+	echo - sargo  (Google Pixel 3a)
 	echo - onyx  (OnePlus X)
 	echo - osprey  (Motorola Moto G3)
 	exit -1
